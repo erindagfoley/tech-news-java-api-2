028d59683fd7f9fc9604cdf42b3394092fee71e7
erin foley <erindagfoley@gmail.com>
2025-04-29 12:39:43 -0400

    please work i tried cloning with ssh

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int EddieAge = 8;
+        double GretaAge = 8.65;
+        double sum = EddieAge + GretaAge;
+        String EddieName = "Eddie";
+        String concat = EddieAge + EddieName;
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
